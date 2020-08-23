09da1d2d24ef56d8656094468ba2d4155e1ba302
Nathan Burns <nathanandreburns@gmail.com>
2020-08-23 23:00:35 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,13 @@
+require "pry"
 require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML(html)
+
+text = doc.css(".headline-26OIBN").text
+
+headings = doc.css(".title-3Kf9MY")
+
+binding.pry
